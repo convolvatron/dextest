fde70018b0f98a57960aad4dce24ba5f168c72e2
Eric Hoffman <eric@iron.io>
2014-09-25 11:22:09 -0700

    single dequeue machine...segmentation fault in the client

--- t1.c
+++ t1.c
@@ -29,8 +29,38 @@ void release_op(worker w, op o) {
 
 
 uint64_t time;
-int enq = 100;
-int deq = 100;
+uint64_t enq = 10000;
+// this guy can go negative
+int64_t deq = 10000;
+uint64_t search_r = 0;
+uint64_t search_w = 0;
+
+uint64_t max_issued; 
+
+#define QUEUE_DEPTH 128
+
+uint64_t queue[QUEUE_DEPTH];
+
+#define QUEUE_EMPTY (0xffffffffffffffffull)
+
+// single writer
+void add_search(uint64_t x)
+{
+    queue[search_w++] = x;
+}
+
+// but multiple readers
+uint64_t remove_search()
+{
+    uint64_t r, w, tag;
+
+    while(r = search_r, w = search_w, tag = queue[r & (QUEUE_DEPTH - 1)], r!= w)
+        if (__sync_bool_compare_and_swap(&search_r, r, r+1))
+            return(tag);
+
+    return(QUEUE_EMPTY);
+}
+
 
 void enqueue(worker w)
 {
@@ -38,6 +68,12 @@ void enqueue(worker w)
     uint64_t key = __sync_fetch_and_add (&time, 1);
     sprintf(o->name, "%08lx", key);
     __sync_fetch_and_sub (&enq, 1);
+
+    printf ("enqueue\n");
+
+    if (enq == 0) 
+        printf ("enqueue last submitted\n");
+
     o->f = release_op;
     uint64_t tag = hyperdex_client_put(w->client, "messages", 
                                        o->name, 8,
@@ -49,23 +85,37 @@ void dequeue(worker);
 
 void delete_complete(worker w, op o)
 {
-    if (o->status == HYPERDEX_CLIENT_NOTFOUND) {
-        dequeue(w);
-    } else {
-        printf ("del complete %d\n", deq);
+    if (o->status == HYPERDEX_CLIENT_SUCCESS) {
+        printf ("del complete %ld\n", deq);
         __sync_fetch_and_sub (&deq, 1);
     }
     release_op(w, o);
 }
 
+void start_delete(worker w, uint64_t t)
+{
+    op od = allocate_op(w);
+    uint64_t size = sprintf(od->name, "%08lx", t);
+
+    printf ("start delete: %d %d\n", deq, t);
+    
+    uint64_t tag = hyperdex_client_del(w->client, "messages", 
+                                       od->name, size,
+                                       &od->status);
+    od->f = delete_complete;
+
+    insert(w->pending, tag, od);
+}
+
+
+void start_search(worker w);
+
 void search_complete(worker w, op o)
 {
-    // ok, well really this is going to get called for each
-    // result from the sorted search. its not all clear 
-    // how this gets cleaned up or terminated
-
     if (o->status == HYPERDEX_CLIENT_SEARCHDONE) {
         release_op(w, o);
+        // we're shutting down
+        if (deq > 0) start_search(w);
         return;
     }
 
@@ -78,30 +128,26 @@ void search_complete(worker w, op o)
 
     if (o->result_size > 1) {
         struct hyperdex_client_attribute *r = &o->result[0];
-        char *k;
-        op od = allocate_op(w);
-        uint64_t tag = hyperdex_client_del(w->client, "messages", 
-                                           r[0].value, r[0].value_sz,
-                                           &od->status);
-        insert(w->pending, tag, od);
-        od->f = delete_complete;
+        uint64_t t;
+        sscanf(r->value, "%08lx", &t);
+        add_search(t);
         hyperdex_client_destroy_attrs((const struct hyperdex_client_attribute *)o->result,
                                       o->result_size);
     }
 }
 
-void dequeue(worker w)
+void start_search(worker w)
 {
-    int result_count = 1;
     op o = allocate_op(w);
     o->f = search_complete;
-
+    int len = QUEUE_DEPTH - (search_w - search_r);
+    printf ("start search %ld %ld %ld\n", search_r, search_w,  len);
     uint64_t tag = hyperdex_client_sorted_search(w->client,
                                                  "messages",
                                                  NULL, 
                                                  0,
                                                  "id",
-                                                 result_count,
+                                                 len,
                                                  0, // minimize
                                                  &o->status,
                                                  (const struct hyperdex_client_attribute **)&o->result,
@@ -121,14 +167,19 @@ void run_worker()
 
     enum hyperdex_client_returncode loop_status;
 
+    // should just be one guy running
+    start_search(&w);
+
     while (enq || (deq > 0) || w.pending->count) {
         if (w.pending->count < CONCURRENCY) {
-            if (enq) {
-                enqueue(&w);
-            } else 
-                if (deq > 0) dequeue(&w);
+            if (enq) enqueue(&w);
+            if (deq > 0) {
+                uint64_t t = remove_search();
+                if (t != QUEUE_EMPTY) 
+                    start_delete(&w, t);
+            }
         }
-
+        
         uint64_t x = hyperdex_client_loop(w.client, 100, &loop_status);
 
         if (loop_status != HYPERDEX_CLIENT_NONEPENDING) {        
